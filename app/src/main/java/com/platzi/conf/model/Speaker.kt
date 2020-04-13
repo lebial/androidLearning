c6adf7367ceb227ec2085c05373b28b283f2e32e
Oswaldo Rangel Suarez <Oswaldo Rangel@tiempodev.local>
2020-04-13 17:52:22 -0500

    First commit

---
+++ app/src/main/java/com/platzi/conf/model/Speaker.kt
@@ -0,0 +1,11 @@
+package com.platzi.conf.model
+
+class Speaker {
+    var name = ""
+    var jobtitle = ""
+    var workspace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+    var category = 0
+}
